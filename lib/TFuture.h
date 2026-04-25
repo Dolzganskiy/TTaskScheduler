88c6d78ebf35f923f18646272a9978994369182f
Данил Лутцев <kepleomax@MacBook-Air-Danil.local>
2026-04-25 18:26:00 +0300

    Reworked Nodes logic set on TFuture

--- lib/TFuture.h
+++ lib/TFuture.h
@@ -2,7 +2,24 @@
 template<typename T>
 class TFuture {
 public:
-    auto Get() {
-        return {};
+    TFuture(std::shared_ptr<NodeBase> node) : npde_(node) {}
+
+    T Get() {
+        node_->Execute();
+
+        if (node_->WasMoved()) {
+            throw std::runtime_error("Already moved");
+        }
+
+        auto& raw = node_->GetRawResult();
+
+        if constexpr (std::is_reference_v<T>) {
+            return std::any_cast<T>(raw);
+        } else {
+            node_->MarkAsMoved();
+            return std::any_cast
+        }
     }
+private:
+    std::shared_ptr<NodeBase>
 };
--- lib/TTask.h
+++ lib/TTask.h
@@ -7,12 +7,14 @@
 #include<optional>
 #include <stdexcept>
 #include <typeinfo>
+#include <any>
 
 struct NodeBase {
     virtual ~NodeBase() = default;
     virtual void Execute() = 0;
-    virtual void* GetRawResult() = 0;
-    virtual const std::type_info& GetResultType() const = 0;
+    virtual std::any& GetRawResult() = 0;
+    virtual bool WasMoved() const = 0;
+    virtual void MarkAsMoved() = 0;
 };
 
 template<typename... Args>
@@ -26,7 +28,6 @@ struct Node<> {
 template<typename T, typename... Args>
 struct Node<T, Args...> {
     static constexpr std::size_t arg_count = 1 + Node<Args...>::arg_count;
-
     Node(T&& arg, Args&&... args) 
         : arg_(std::forward<T>(arg)), 
           tail_(std::forward<Args>(args)...) 
@@ -55,12 +56,7 @@ struct NodeGet<0, Node<T, Args...>> {
 
 template<typename Task, typename... Args> 
 struct TaskNode : NodeBase {
-
     using result_type = std::invoke_result_t<Task, Args...>;
-
-    TaskNode& operator=(const TaskNode& other) {
-        task
-    }
 
     TaskNode(Task&& task, Args&&... args) 
         : task_(std::forward<Task>(task)), 
@@ -68,55 +64,66 @@ struct TaskNode : NodeBase {
     {}
 
     void Execute() override {
+        if (executed_) return;
 
-        if (executed_) {
-            return;
-        }
-
-        if constexpr (std::is_void_v<result_type>) {
-            ExecuteImpl(std::make_index_sequence<sizeof...(Args)>{});
-        } else {
-            result_ = ExecuteImpl(std::make_index_sequence<sizeof...(Args)>{});
-        }
-
+        result_ = ExecuteImpl(std::make_index_sequence<sizeof...(Args)>{});
         executed_ = true;
     }
 
-    void* GetRawResult() override {
-        if constexpr (std::is_void_v<result_type>) {
-            return nullptr;
-        } else {
-            return result_ ? &(*result_) : nullptr;
-        }
-    } 
-
-    const std::type_info& GetResultType() const override {
-        return typeid(result_type);
-    }
-
-    void MakeNewTask()
+    std::any& GetRawResult() override { return result_; } 
+    bool WasMoved() const override { return moved_; }
+    void MarkAsMoved() override { moved_ = true; }
 
 private:
     Task task_;
     Node<Args...> args_;
 
     bool executed_ = false;
-    std::optional<result_type> result_;
+    bool moved_ = false;
+    std::any result_;
 
     template<std::size_t... Ind>
     result_type ExecuteImpl(std::index_sequence<Ind...>) {
         return task_(NodeGet<Ind, Node<Args...>>::Get(args_)...);
     }
-
 };
 
-template<typename Task>
-void UpdateTask(Task task) {
+template<typename PrevType, typename NewTask>
+struct ChainNode : NodeBase {
+public:
+    using result_type = std::invoke_result_t<NewTask, PrevType>;
 
-}
+    ChainNode(std::shared_ptr<NodeBase> prev, NewTask&& task) 
+        : prev_(prev), task_(std::forward<NewTask>(task))
+    {}
+
+    void Execute() override {
+        if (executed_) return;
+
+        prev_->Execute();
+        PrevType prev_res = std::any_cast<PrevType>(prev_->GetRawResult());
+
+        result_ = std::invoke(task_; std::forward<PrevType>(prev_res));
+        executed_ = true;
+    }
+
+    void* GetRawResult() override { return &result_; }
+    bool WasMoved() const override { return moved_; }
+    void MarkAsMoved() override { moved_ = true; }
+
+private:
+    std::shared_ptr<NodeBase> prev_;
+    NewTask task_;
+    bool executed_ = false;
+    bool moved_ = false;
+    std::any result_;
+};
+
 
 class TTask {
 public:
+
+    TTask() = default;
 
     template<typename Task, typename... Args>
     TTask(Task&& task, Args&&... args) {
@@ -135,17 +142,17 @@ public:
 
         node_->Execute();
 
-        if (node_->GetResultType() != typeid(T)) {
-            throw std::runtime_error("Wrong result type requested");
-        }
-
-        void* raw = node_->GetRawResult();
+        std::any* raw = static_cast<std::any*>(node_->GetRawResult());
 
         if (!raw) {
-            throw std::runtime_error("Task has void result");
+            throw std::runtime_error("Task returned void");
+        }
+
+        if (raw->type() != typeid(T)) {
+            throw std::runtime_error("Wrong result type");
         }
     
-        return std::move(*static_cast<T*>(raw));
+        return std::any_cast<T>(*raw);
     }
 
     template<typename T>
@@ -153,9 +160,20 @@ public:
         return TFuture<T>{};
     }
 
-    template<typename Task, typename... Args>
-    TTask apply(Task task) {
+    template<typename NewTask>
+    TTask Apply(NewTask&& task) {
 
+        if (!node_) {
+            throw std::runtime_error("No task to apply to");
+        }
+
+        TTask new_t;
+
+        new_t.node_ = std::make_unique<ChainNode<NewTask>>(
+                    std::move(node_), std::forward<NewTask>(task)
+        );
+
+        return new_t;
     }
 
     void Execute() {
