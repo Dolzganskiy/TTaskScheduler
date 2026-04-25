cd6eb269e5d1f679262922aa0774350b57ec96b4
Данил Лутцев <kepleomax@MacBook-Air-Danil.local>
2026-04-25 19:27:14 +0300

    Make my any and invoke

---
+++ lib/utility/TAny.h
@@ -0,0 +1,40 @@
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+class Any {
+public:
+    Any() = default;
+
+    template<typename T>
+    Any(T&& v) : ptr_(std::make_unique<Holder<std::decay_t<T>>>(std::forward<T>(v))) {}
+
+    bool HasValue() const { return ptr_ != nullptr; }
+
+    template<typename T>
+    T& Cast() {
+        auto* derived = dynamic_cast<Holder<std::decay_t<T>>*>(ptr.get());
+        if (!derived) throw std::runtime_error("Any: Wrong type cast!");
+        return derived->value;
+    }
+
+    template<typename T>
+    T Move_cast() {
+        auto* derived = dynamic_cast<Holder<std::decay_t<T>>*>(ptr.get());
+        if (!derived) throw std::runtime_error("Any: Wrong type cast!");
+        return std::move(derived->value);
+    }
+private:
+    struct Base {
+        virtual ~Base() = default;
+    };
+
+    template<typename T>
+    struct Holder : Base {
+        T value_;
+        Holder(T&& v) : value_(std::forward<T>(v)) {}
+        Holder(const T& v) : value_(v) {}
+    };
+    
+    std::unique_ptr<Base> ptr_;
+};
---
+++ lib/utility/TInvoke.h
@@ -0,0 +1,18 @@
+#include <utility>
+#include <type_traits>
+
+template<typename F, typename... Args>
+decltype(auto) Invoke(F&& f, Args&&... args) {
+    if constexpr (std::is_member_function_pointer_v<std::decay_t<F>>) {
+        auto helper = [](auto&& obj, auto&& method, auto&&... params) -> decltype(auto) {
+            if constexpr (std::is_pointer_v<std::decay_t<decltype(obj)>>) {
+                return (obj->*method)(std::forward<decltype(params)>(params)...);
+            } else {
+                return (obj.*method)(std::forward<decltype(params)>(params)...);
+            }
+        };
+        return helper(std::forward<Args>(args)..., std::forward<F>(f));
+    } else {
+        return std::forward<F>(f)(std::forward<Args>(args)...);
+    }
+}
